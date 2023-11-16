f5bd45e45187e17fb693f8a77e40c3bfdf8cb50c
Hitesh Gorantla <74699575+GSHitesh@users.noreply.github.com>
2023-11-16 08:08:17 +0530

    Added parking lot files

---
+++ vehicle.py
@@ -0,0 +1,7 @@
+"""Vehicle Module to store vehicle info"""
+
+class Vehicle:
+    """Vehicle class"""
+
+    def __init__(self, vehicle_number: str) -> None:
+        self.vehicle_number = vehicle_number
